93661711ff41e88e2632d44ae42bc5562a66651b
Krystian Jabłoński <Krystian Jaboski>
2017-09-27 19:27:42 +0200

    Adding scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var value = prompt('Enter value - please, only numbers (or not)!');
+var a = 3,
+    b = 5,
+    c = 10,
+    score = (a * value) - (8 * a * b) + (b * b);
+
+if (score > 0) {
+    alert('Score is ' + score + ' and that is more than zero.');
+    console.log('Score is ' + score + ' and that is more than zero.');
+}
+else if (score < 0) {
+    alert('Score is ' + score + ' and that is less than zero.');
+    console.log('Score is ' + score + ' and that is less than zero.');
+}
+else if (score == 0) {
+    alert('Score is equal to zero');
+    console.log(score);
+}
+else {
+    alert('Score is beyond math and something went wrong!');
+    console.log('Score is beyond math and something went wrong!');
+}

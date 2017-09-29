a3d9d9a640c57f4d35f50f3842d702f8859d2cb0
Krystian Jabłoński <krystianpaweljablonski@gmail.com>
2017-09-29 23:32:05 +0200

    Update scripts.js
    Triangle Area calc.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,27 @@
-var name = prompt('Enter your name'),
-a = 5,
-h = 3,
-triangleArea = a*h/2;
-// Comment
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
-// Triangle
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+function getTriangleArea(a, h) {
+  if ((a < 0) && (h > 0)) {
+      return ("A mniejsze od zera, ale H jest spoko");
+  }
+  else if ((a > 0) && (h < 0)) {
+      return ("A jest spoko, ale H mniejsze od zera");
+  }
+  else if ((a === 0) && (h === 0)) {
+      return("A równe zero, H równe zero")
+  }
+  return a * h/2;
+}
+var triangleArea1 = getTriangleArea(-5, 5);
+  console.log(triangleArea1);
+
+var triangleArea2 = getTriangleArea(4, 2);
+  console.log(triangleArea2);
+
+var triangleArea3 = getTriangleArea(10, -2);
+  console.log(triangleArea3);
+
+var triangleArea4 = getTriangleArea (0, 0);
+  console.log(triangleArea4);
+
+
+
+

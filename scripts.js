3084b0164a655ac67860c08e2cf63da988898677
Krystian Jabłoński <krystianpaweljablonski@gmail.com>
2017-09-30 00:54:19 +0200

    adding one extra logic event

--- scripts.js
+++ scripts.js
@@ -8,6 +8,9 @@ function getTriangleArea(a, h) {
   else if ((a === 0) && (h === 0)) {
       return("A equals zero, H equals zero")
   }
+  else if ((a <0) && (h <0)) {
+      return("A is less than zero, so is H");
+  }
   return a * h/2;
 }
 var triangleArea1 = getTriangleArea(-5, 5);
@@ -22,6 +25,5 @@ var triangleArea3 = getTriangleArea(10, -2);
 var triangleArea4 = getTriangleArea (0, 0);
   console.log(triangleArea4);
 
-
-
-
+var triangleArea5 = getTriangleArea (-1, -1);
+  console.log(triangleArea5);

46f846532161f113e36263c49e0788cea9d96d26
Krystian Jabłoński <krystianpaweljablonski@gmail.com>
2017-09-30 22:42:08 +0200

    Update scripts.js
    Fixes needed!

--- scripts.js
+++ scripts.js
@@ -1,10 +1,19 @@
-var name = prompt('Enter your name'),
-a = 5,
-h = 3,
-triangleArea = a*h/2;
-// Comment
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
-// Triangle
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var maleArray = ['John', 'Joe', 'Jack'];
+    femaleArray = ['Kat', 'Lindsay', 'Annie'];
+    allNamesArray = maleArray.concat(femaleArray);
+
+console.log(allNamesArray);
+
+var maleArrayExtended = maleArray.push('Marian')
+
+console.log(maleArray);
+
+var name = '';
+      switch (name) {
+        case 'Marian':
+var maleWithoutMarian = maleArray.filter(function(name) {
+       return name != 'Marian';
+    });
+    break;
+      }
+console.log(maleArray);

8fc2a55c520b76c5f6ce854f32725cc0f33dbce1
Krystian Jabłoński <krystianpaweljablonski@gmail.com>
2017-10-01 12:01:10 +0200

    Update scripts.js
    improvements

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Moje próby ze switchem
+
 var maleArray = ['John', 'Joe', 'Jack'];
     femaleArray = ['Kat', 'Lindsay', 'Annie'];
     allNamesArray = maleArray.concat(femaleArray);
@@ -17,3 +19,27 @@ var maleWithoutMarian = maleArray.filter(function(name) {
     break;
       }
 console.log(maleArray);
+
+// Wersja z funkcjami poniżej
+
+var namesAbleToRemove = ['Marian', 'Marek'];
+var allNames = ['Marian', 'Jacek', 'Prezes'];
+
+//   funkcja do sprawdzenia czy i jesli tablica namesAbleToRemove zawiera w sobie 'Prezesa'
+//   literalnie czyt. jesli tablica zwraca -1(rozny od -1 czyli jest obecny, bo jak by bylo -1 to by go nie bylo w tablicy)
+//   to tego elementu nie ma w niej, jesli jest to zwroci 1. W tym przypadku mowa o sytuacji, gdzie jest zaprzeczeniem nieobecnosci
+//   czyli !== -1 oznacza obecnosc elementu w tablicy. Jesli zwroci rozne od -1 to jest i mozna go wywalic, jesli zwroci -1 to nie mozna bo go nie ma.
+function getArrWithoutName (name) {
+  if (namesAbleToRemove.indexOf(name) !== -1) {
+    return allNames.filter(function(i) {return i !== name});
+  }
+  return allNames;
+}
+
+function addNameAbleToRemove(name) {
+  namesAbleToRemove.push(name)
+}
+
+console.log(getArrWithoutName('Prezes'));
+addNameAbleToRemove('Prezes');
+console.log(getArrWithoutName('Prezes'));

78d26dcd21c73933518bff3467a215ca9bee4f62
Krystian Jabłoński <krystianpaweljablonski@gmail.com>
2017-10-02 21:54:30 +0200

    Update scripts.js
    Star Tree!

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-var name = prompt('Enter your name'),
-a = 5,
-h = 3,
-triangleArea = a*h/2;
-// Comment
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
-// Triangle
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+let starTree = function(row){
+ for (i = 0; i<row; i++){
+    let star = ''
+    for(j= 0; j<i; j++){
+      star += '*'
+    }
+      console.log(star)
+  }
+}
+starTree(7)

f43a66eab7ff98116c1276496ae6e1caf057a8f8
Krystian Jabłoński <krystianpaweljablonski@gmail.com>
2017-10-03 11:52:20 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
-let starTree = function(row){
- for (i=1; i<=row; i++){
-    let star = ''
-    for(j=0; j<i; j++){
-      star += '*' 
-//       same as '' + * = '*'
-    }
-      console.log(star)
-  }
+function drawTree(treeHeight) {
+	for (var i = 0; i < treeHeight; i++) {
+		var star = '';
+		var whitespace = '';
+		for (var j = 0; j < 2 * i + 1; j++) {
+			star += '*';
+		}
+		for (var k = i; k < treeHeight; k++) {
+			whitespace += "j";
+		}
+		console.log(whitespace + star);
+	}
 }
-starTree(10)
+
+drawTree(prompt('Wpisz wysokość choinki:'));

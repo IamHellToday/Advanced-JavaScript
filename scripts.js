6dba14613187a9582cffb17274f13028340cf296
Krystian Jabłoński <krystianpaweljablonski@gmail.com>
2017-10-03 21:45:17 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-var name = prompt('Enter your name'),
-a = 5,
-h = 3,
-triangleArea = a*h/2;
-// Comment
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
-// Triangle
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var iterationOnDom = function () {
+    var variableWithElements = document.getEelementsByClassName("button");
+    var lenghtOfvariableWithElements = variableWithElements.length;
+    
+    for (var i = 0; i < lenghtOfvariableWithElements; i++) {
+        alert(variableWithElements.innerText);
+    }
+        
+};
+console.log(iterationOnDom());
